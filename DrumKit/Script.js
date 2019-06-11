40bc8b18559830dd9b5ffcb40ef254e95e43e8f1
Utkarsh Kumar <utkarsh9799@gmail.com>
2019-06-11 23:33:38 +0530

    Fixed audio playback delay on repeated event

--- DrumKit/Script.js
+++ DrumKit/Script.js
@@ -3,5 +3,6 @@ window.addEventListener('keydown', playSound);
 function playSound(e) 
 {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    audio.currentTime = 0;
     audio.play();
 }
