88b40bf8dc79e4ee76e50cf0377c4507aac62a4f
Utkarsh Kumar <utkarsh9799@gmail.com>
2019-06-11 23:52:11 +0530

    Added transition on keydown

--- DrumKit/Script.js
+++ DrumKit/Script.js
@@ -3,6 +3,8 @@ window.addEventListener('keydown', playSound);
 function playSound(e) 
 {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    key.classList.add("transition");
     audio.currentTime = 0;
     audio.play();
 }
