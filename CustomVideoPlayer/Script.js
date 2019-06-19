3f5749339d2a6e229be2eefca90724da5f12ad6f
Utkarsh Kumar <utkarsh9799@gmail.com>
2019-06-19 21:11:20 +0530

    Added video seeking functionality

--- CustomVideoPlayer/Script.js
+++ CustomVideoPlayer/Script.js
@@ -53,3 +53,15 @@ function rangeUpdate()
 {
     video[this.name] = this.value;
 }
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
+
+function scrub(e) 
+{
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+}
