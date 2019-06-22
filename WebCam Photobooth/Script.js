74eb555fc920b38782d093e5c963706d2c1ddfdf
Utkarsh Kumar <utkarsh9799@gmail.com>
2019-06-23 00:43:52 +0530

    Defined a function for red pixels

--- WebCam Photobooth/Script.js
+++ WebCam Photobooth/Script.js
@@ -40,4 +40,13 @@ function picToCanvas() {
     }, 16);
 }
 
+function redEffect(pixels) {
+    for (let i = 0; i < pixels.data.length; i+=4) {
+      pixels.data[i + 0] = pixels.data[i + 0] + 200; // RED
+      pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
+      pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
+    }
+    return pixels;
+}
+
 video.addEventListener('canplay', picToCanvas);
