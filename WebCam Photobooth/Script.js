70fe2f652c752ab458f69d680f37e0ef8bf67b77
Utkarsh Kumar <utkarsh9799@gmail.com>
2019-06-23 00:47:44 +0530

    Made a function to take photo

--- WebCam Photobooth/Script.js
+++ WebCam Photobooth/Script.js
@@ -83,4 +83,18 @@ function greenScreen(pixels) {
     return pixels;
 }
 
+function takePhoto() {
+    // played the sound
+    snap.currentTime = 0;
+    snap.play();
+  
+    // take the data out of the canvas
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download', 'handsome');
+    link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
+    strip.insertBefore(link, strip.firstChild);
+}
+
 video.addEventListener('canplay', picToCanvas);
